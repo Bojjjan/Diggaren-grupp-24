675f8847c9da5042fa38c82319027f4e75df8793
Douglas <doggeat@gmail.com>
2024-12-18 14:37:36 +0100

    Fixed imports in sveriges_radio_api and server

--- src/main/api/sveriges_radio_api.py
+++ src/main/api/sveriges_radio_api.py
@@ -1,4 +1,4 @@
-from src.main.models.track import Track
+from main.models.track import Track
 from datetime import datetime, timedelta
 import requests
 
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 from flask import Flask, jsonify, Response
 
 from main.api.sveriges_radio_api import SvergiesRadioApi
